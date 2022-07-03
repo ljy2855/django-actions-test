6026d673f796ec4ee76d035080eb58dd0e1a61b5
ljy2855 <ljy2855@naver.com>
2022-07-03 17:15:12 +0900

    Add CustomUser testCase

--- mysite/user/models.py
+++ mysite/user/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+class CustomUser(models.Model):
+    user = models.OneToOneField(User,on_delete=models.CASCADE)
+    created_date = models.DateField(auto_now=True)
 
 # Create your models here.
--- mysite/user/tests.py
+++ mysite/user/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from models import CustomUser
+from django.contrib.auth.models import User
+class UserTest(TestCase):
+    @classmethod
+    def setUpTestData(cls) -> None:
+        u, created = User.objects.create("testname","test@example.com","password")
+        if created:
+            CustomUser.objects.create(user=u)
+        return super().setUpTestData()
 
+    def testCheckAllUser(self):
+        self.assertIsNone(CustomUser.objects.all())
 # Create your tests here.
